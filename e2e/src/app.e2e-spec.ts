e52c38523703a22297895f1fc1145a50c6097b98
Rogério Alonso <69876702+rogerioloualonso@users.noreply.github.com>
2023-11-17 14:34:11 -0300

    Retirada de referências

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('eagenda app is running!');
+    expect(page.getTitleText()).toEqual('sgai app is running!');
   });
 
   afterEach(async () => {
